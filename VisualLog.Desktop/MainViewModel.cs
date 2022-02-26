81c924d33f847fe2fb29c894cc2c9e60019c2590
diamtr <67097638+diamtr@users.noreply.github.com>
2022-02-27 00:23:41 +0300

    Add OpenFileCommand

--- VisualLog.Desktop/MainViewModel.cs
+++ VisualLog.Desktop/MainViewModel.cs
@@ -2,5 +2,19 @@
 {
   public class MainViewModel
   {
+    public Command OpenFileCommand { get; private set; }
+
+    public MainViewModel()
+    {
+      this.InitCommands();
+    }
+
+    private void InitCommands()
+    {
+      this.OpenFileCommand = new Command(
+        x => { },
+        x => true
+        );
+    }
   }
 }
