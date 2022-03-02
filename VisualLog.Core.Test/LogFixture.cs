01309afd0dfc7e1a2c6f21179842becb5f6c518b
diamtr <67097638+diamtr@users.noreply.github.com>
2022-03-02 23:42:40 +0300

    Simple read test

--- VisualLog.Core.Test/LogFixture.cs
+++ VisualLog.Core.Test/LogFixture.cs
@@ -1,14 +1,27 @@
 ﻿using NUnit.Framework;
+using System.IO;
 
 namespace VisualLog.Core.Test
 {
   [TestFixture]
   public class LogFixture
   {
+    private const string TestLogsDirName = @"test_logs";
+
     [Test]
     public void Read()
     {
-      Assert.Fail();
+      var log = new Log();
+      var path = Path.Combine(TestLogsDirName, "0000.log");
+      log.Read(path);
+
+      Assert.AreEqual(3, log.Messages.Count);
+      Assert.AreEqual(0, log.Messages[0].Number);
+      Assert.AreEqual("line0", log.Messages[0].RawValue);
+      Assert.AreEqual(1, log.Messages[1].Number);
+      Assert.AreEqual("line1", log.Messages[1].RawValue);
+      Assert.AreEqual(2, log.Messages[2].Number);
+      Assert.AreEqual("line2", log.Messages[2].RawValue);
     }
   }
 }
