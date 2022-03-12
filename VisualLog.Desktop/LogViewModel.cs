ae3aa036ca85582c84346e702e9eaf70ee158744
diamtr <67097638+diamtr@users.noreply.github.com>
2022-03-12 23:05:13 +0300

    Read log in different encodings

--- VisualLog.Desktop/LogViewModel.cs
+++ VisualLog.Desktop/LogViewModel.cs
@@ -22,6 +22,7 @@ namespace VisualLog.Desktop
       }
     }
 
+    private string logPath;
     private Log log;
     private string selectedEncoding;
 
@@ -29,6 +30,17 @@ namespace VisualLog.Desktop
     {
       this.LogMessages = new List<string>();
       this.Encodings = new List<string>();
+      this.InitEncodings();
+      this.PropertyChanged += SelectedEncoding_PropertyChanged;
+    }
+
+    public void ReadLog()
+    {
+      if (string.IsNullOrWhiteSpace(this.logPath) ||
+          !System.IO.File.Exists(this.logPath))
+        return;
+
+      this.ReadLog(this.logPath);
     }
 
     public void ReadLog(string path)
@@ -37,19 +49,35 @@ namespace VisualLog.Desktop
           !System.IO.File.Exists(path))
         return;
 
-      var encs = Encoding.GetEncodings();
-      this.Encodings.AddRange(encs.Select(x => this.GetEncodingDisplayName(x.GetEncoding())));
-      this.OnPropertyChanged(nameof(this.Encodings));
-      this.SelectedEncoding = this.GetEncodingDisplayName(Encoding.UTF8);
-      this.log = new Log();
-      this.log.Read(path);
+      this.logPath = path;
+      var encoding = Encoding.Default;
+      if (!string.IsNullOrWhiteSpace(this.SelectedEncoding))
+        encoding = Encoding.GetEncoding(int.Parse(this.SelectedEncoding.Split(' ')[0]));
+      this.log = new Log(encoding);
+      this.log.Read(this.logPath);
       this.LogMessages = this.log.Messages.Select(x => x.RawValue).ToList();
       this.OnPropertyChanged(nameof(this.LogMessages));
+    }
+
+    private void InitEncodings()
+    {
+      var encodings = Encoding.GetEncodings();
+      this.Encodings.AddRange(encodings.Select(x => this.GetEncodingDisplayName(x.GetEncoding())));
+      this.OnPropertyChanged(nameof(this.Encodings));
+      this.SelectedEncoding = this.GetEncodingDisplayName(Encoding.UTF8);
     }
 
     private string GetEncodingDisplayName(Encoding encoding)
     {
       return $"{encoding.CodePage} {encoding.WebName} {encoding.EncodingName}";
+    }
+
+    private void SelectedEncoding_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+      if (sender == null || e.PropertyName != nameof(SelectedEncoding))
+        return;
+
+      this.ReadLog();
     }
   }
 }
