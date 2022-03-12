a9666ed7793c257d702ec7426e44ac7fb1660f3c
diamtr <67097638+diamtr@users.noreply.github.com>
2022-03-12 22:43:40 +0300

    Open log with encoding

--- VisualLog.Core/Log.cs
+++ VisualLog.Core/Log.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace VisualLog.Core
 {
   public class Log
   {
     public List<Message> Messages { get; set; }
+    public Encoding Encoding { get; private set; }
 
     public Log()
     {
       this.Messages = new List<Message>();
     }
 
+    public Log(Encoding encoding) : this()
+    {
+      this.Encoding = encoding;
+    }
+
     public void Read(string path)
     {
-      var lines = System.IO.File.ReadAllLines(path).ToList();
+      var lines = new List<string>();
+      if (this.Encoding != null)
+        lines = File.ReadAllLines(path, this.Encoding).ToList();
+      else
+        lines = File.ReadAllLines(path).ToList();
       foreach (var line in lines)
         this.Messages.Add(new Message() { Number = lines.IndexOf(line), RawValue = line });
     }
--- VisualLog.Desktop/LogViewModel.cs
+++ VisualLog.Desktop/LogViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using VisualLog.Core;
 
 namespace VisualLog.Desktop
@@ -7,12 +8,27 @@ namespace VisualLog.Desktop
   public class LogViewModel : ViewModelBase
   {
     public List<string> LogMessages { get; set; }
+    public List<string> Encodings { get; set; }
+    public string SelectedEncoding
+    {
+      get
+      {
+        return this.selectedEncoding;
+      }
+      set
+      {
+        this.selectedEncoding = value;
+        this.OnPropertyChanged();
+      }
+    }
 
     private Log log;
+    private string selectedEncoding;
 
     public LogViewModel()
     {
       this.LogMessages = new List<string>();
+      this.Encodings = new List<string>();
     }
 
     public void ReadLog(string path)
@@ -21,10 +37,19 @@ namespace VisualLog.Desktop
           !System.IO.File.Exists(path))
         return;
 
+      var encs = Encoding.GetEncodings();
+      this.Encodings.AddRange(encs.Select(x => this.GetEncodingDisplayName(x.GetEncoding())));
+      this.OnPropertyChanged(nameof(this.Encodings));
+      this.SelectedEncoding = this.GetEncodingDisplayName(Encoding.UTF8);
       this.log = new Log();
       this.log.Read(path);
       this.LogMessages = this.log.Messages.Select(x => x.RawValue).ToList();
       this.OnPropertyChanged(nameof(this.LogMessages));
+    }
+
+    private string GetEncodingDisplayName(Encoding encoding)
+    {
+      return $"{encoding.CodePage} {encoding.WebName} {encoding.EncodingName}";
     }
   }
 }
