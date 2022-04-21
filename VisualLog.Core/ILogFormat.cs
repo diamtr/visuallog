6e8f309300b3b2f3d66c6edddb682224d8bc20e8
diamtr <67097638+diamtr@users.noreply.github.com>
2022-04-21 21:45:15 +0300

    Add: ILogFormat, IMessageFormatter

---
+++ VisualLog.Core/ILogFormat.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace VisualLog.Core
+{
+  public interface ILogFormat
+  {
+    public string Name { get; set; }
+    public List<IMessageFormatter> Formatters { get; set; }
+    public IDictionary<string, string> Deserialize(Message message);
+  }
+}
