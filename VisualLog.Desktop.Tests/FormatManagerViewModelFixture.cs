c8f9402e1d9a959b0a13a6b21b02406486387320
diamtr <67097638+diamtr@users.noreply.github.com>
2022-07-25 23:35:31 +0300

    Test

---
+++ VisualLog.Desktop.Tests/FormatManagerViewModelFixture.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using VisualLog.Desktop.FormatManager;
+
+namespace VisualLog.Desktop.Tests
+{
+  [TestFixture]
+  public class FormatManagerViewModelFixture
+  {
+    [Test]
+    public void CreateNewFormat()
+    {
+      var vm = new FormatManagerViewModel();
+      CollectionAssert.IsEmpty(vm.Formats);
+      Assert.IsNull(vm.SelectedFormat);
+
+      vm.CreateFormat();
+      CollectionAssert.IsNotEmpty(vm.Formats);
+      Assert.AreEqual(1, vm.Formats.Count);
+      Assert.IsNotNull(vm.SelectedFormat);
+
+      vm.CreateFormatCommand.Execute(null);
+      CollectionAssert.IsNotEmpty(vm.Formats);
+      Assert.AreEqual(2, vm.Formats.Count);
+      Assert.IsNotNull(vm.SelectedFormat);
+    }
+  }
+}
