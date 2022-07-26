d4529c62c8f13f4ef0f80f6a33d617a3f4f7ed8c
diamtr <67097638+diamtr@users.noreply.github.com>
2022-07-26 23:17:58 +0300

    Show Message Formatter

---
+++ VisualLog.Desktop.Tests/LorFormatViewModelFixture.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using VisualLog.Core;
+using VisualLog.Desktop.FormatManager;
+
+namespace VisualLog.Desktop.Tests
+{
+  [TestFixture]
+  public class LorFormatViewModelFixture
+  {
+    [Test]
+    public void CreateNewLogFormatViewModel()
+    {
+      var vm = new LogFormatViewModel();
+      Assert.IsNull(vm.Format);
+      Assert.IsNull(vm.SelectedFormatter);
+
+      var format = new LogFormat();
+      vm = new LogFormatViewModel(format);
+      Assert.AreEqual(format, vm.Format);
+      Assert.IsNull(vm.SelectedFormatter);
+
+    }
+
+    [Test]
+    public void AddNewFormatter()
+    {
+      var format = new LogFormat();
+      var formatVM = new LogFormatViewModel(format);
+      formatVM.AddNewFormatter();
+      Assert.AreEqual(1, formatVM.Formatters.Count);
+      formatVM.AddNewFormatterCommand.Execute(null);
+      Assert.AreEqual(2, formatVM.Formatters.Count);
+    }
+  }
+}
--- VisualLog.Desktop/FormatManager/LogFormatViewModel.cs
+++ VisualLog.Desktop/FormatManager/LogFormatViewModel.cs
@@ -1,4 +1,5 @@
-﻿using VisualLog.Core;
+﻿using System.Collections.ObjectModel;
+using VisualLog.Core;
 
 namespace VisualLog.Desktop.FormatManager
 {
@@ -9,7 +10,40 @@ namespace VisualLog.Desktop.FormatManager
       get { return this.format; }
       set { this.format = value; this.OnPropertyChanged(); }
     }
+    public ObservableCollection<MessageFormatterViewModel> Formatters { get; set; }
+    public MessageFormatterViewModel SelectedFormatter
+    {
+      get { return this.selectedFormatter; }
+      set { this.selectedFormatter = value; this.OnPropertyChanged(); }
+    }
+    public Command AddNewFormatterCommand { get; private set; }
 
     private LogFormat format;
+    private MessageFormatterViewModel selectedFormatter;
+
+    public LogFormatViewModel(LogFormat format) : this()
+    {
+      this.format = format;
+    }
+
+    public LogFormatViewModel()
+    {
+      this.Formatters = new ObservableCollection<MessageFormatterViewModel>();
+      this.InitCommands();
+    }
+
+    public void AddNewFormatter()
+    {
+      var formatterViewModel = new MessageFormatterViewModel();
+      this.Formatters.Add(formatterViewModel);
+    }
+
+    private void InitCommands()
+    {
+      this.AddNewFormatterCommand = new Command(
+        x => { this.AddNewFormatter(); },
+        x => true
+        );
+    }
   }
 }
