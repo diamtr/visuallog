4051dd5bc9dc7bc58d5b3c8a2052a2d33a2afeca
diamtr <67097638+diamtr@users.noreply.github.com>
2022-07-26 23:31:01 +0300

    Simple MessageFormatterViewModel

--- VisualLog.Desktop/FormatManager/MessageFormatterViewModel.cs
+++ VisualLog.Desktop/FormatManager/MessageFormatterViewModel.cs
@@ -1,7 +1,25 @@
-﻿namespace VisualLog.Desktop.FormatManager
+﻿using VisualLog.Core;
+
+namespace VisualLog.Desktop.FormatManager
 {
   public class MessageFormatterViewModel : ViewModelBase
   {
+    public MessageFormatter Formatter
+    {
+      get { return this.formatter; }
+      set { this.formatter = value; this.OnPropertyChanged(); }
+    }
 
+    private MessageFormatter formatter;
+
+    public MessageFormatterViewModel(MessageFormatter formatter) : this()
+    {
+      this.formatter = formatter;
+    }
+
+    public MessageFormatterViewModel()
+    {
+
+    }
   }
 }
