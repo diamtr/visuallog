3847b744da7ca0cef31e5604a9e4fe0281e3a386
diamtr <67097638+diamtr@users.noreply.github.com>
2022-09-08 22:35:08 +0300

    Save Command Test

--- VisualLog.Desktop.Tests/LorFormatViewModelFixture.cs
+++ VisualLog.Desktop.Tests/LorFormatViewModelFixture.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using System.IO;
+using System.Linq;
 using VisualLog.Core;
 using VisualLog.Desktop.FormatManager;
 
@@ -36,10 +38,20 @@ namespace VisualLog.Desktop.Tests
     public void SaveFormat()
     {
       var fileName = @"VisualLog.Formats.Test.json";
-      Core.Formats.InitFrom(fileName);
+      if (File.Exists(fileName))
+        File.Delete(fileName);
+
+      Formats.InitFrom(fileName);
       var format = new LogFormat() { Name = "test format" };
       var formatVM = new LogFormatViewModel(format);
       formatVM.SaveFormat();
+      var formats = Formats.Read();
+      Assert.AreEqual(1, formats.Count());
+      Assert.AreEqual(1, formats.First().Id);
+      Assert.AreEqual("test format", formats.First().Name);
+
+      if (File.Exists(fileName))
+        File.Delete(fileName);
     }
   }
 }
