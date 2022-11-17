7a1ea08f978c1960bb1d83d91836ac35f69ed382
diamtr <67097638+diamtr@users.noreply.github.com>
2022-11-18 00:51:42 +0300

    Add ReadLogUpdates test

--- VisualLog.Core.Test/LogFixture.cs
+++ VisualLog.Core.Test/LogFixture.cs
@@ -137,6 +137,7 @@ namespace VisualLog.Core.Test
     public void StartFollowTail()
     {
       var path = Path.Combine(TestLogsDirName, "followtail.log");
+      File.WriteAllText(path, null);
       var log = new Log(path);
       CollectionAssert.IsEmpty(log.Messages);
 
@@ -155,6 +156,28 @@ namespace VisualLog.Core.Test
       Assert.AreEqual(2, log.Messages.Count);
       Assert.AreEqual("xx.xx.xxxx xx:xx:xx.xxxx Trace First update followtail.log", log.Messages[0].RawValue);
       Assert.AreEqual("xx.xx.xxxx xx:xx:xx.xxxx Trace Second update followtail.log", log.Messages[1].RawValue);
+
+      File.WriteAllText(path, null);
+    }
+
+    [Test]
+    public void ReadLogUpdates()
+    {
+      var path = Path.Combine(TestLogsDirName, "followtail.log");
+      File.WriteAllText(path, null);
+      var log = new Log(path);
+      CollectionAssert.IsEmpty(log.Messages);
+
+      File.AppendAllLines(path, new List<string>() { "xx.xx.xxxx xx:xx:xx.xxxx Trace First update followtail.log" });
+      System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(5));
+
+      Assert.DoesNotThrow(() => { log.ReadLogUpdates(null, new FileSystemEventArgs(WatcherChangeTypes.Changed, TestLogsDirName, "followtail.log")); });
+
+      CollectionAssert.IsNotEmpty(log.Messages);
+      Assert.AreEqual(1, log.Messages.Count);
+      Assert.AreEqual("xx.xx.xxxx xx:xx:xx.xxxx Trace First update followtail.log", log.Messages[0].RawValue);
+
+      File.WriteAllText(path, null);
     }
   }
 }
