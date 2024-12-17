d08bd64a91999dd421113d3b9da876a848b8cb8f
Дмитриев Иван (Dmitriev_IA) <Dmitriev_IA@directum.ru>
2024-12-17 15:02:46 +0400

    Remove horizontal selected line view

--- VisualLog.Desktop/LogManager/LogViewStateViewModel.cs
+++ VisualLog.Desktop/LogManager/LogViewStateViewModel.cs
@@ -4,12 +4,8 @@
   {
     public bool ShowSearchPanel
     {
-      get
-      {
-        return this.showSearchPanel;
-      }
-      set
-      {
+      get { return this.showSearchPanel; }
+      set {
         this.showSearchPanel = value;
         this.OnPropertyChanged();
       }
@@ -17,42 +13,17 @@
     private bool showSearchPanel;
     public bool ShowSelectedMessageVertical
     {
-      get
-      {
-        return this.showSelectedMessagesVertical;
-      }
-      set
-      {
+      get { return this.showSelectedMessagesVertical; }
+      set {
         this.showSelectedMessagesVertical = value;
-        if (this.showSelectedMessagesVertical && this.showSelectedMessagesHorizontal)
-          this.showSelectedMessagesHorizontal = false;
         this.OnPropertyChanged();
       }
     }
     private bool showSelectedMessagesVertical;
-    public bool ShowSelectedMessageHorizontal
-    {
-      get
-      {
-        return this.showSelectedMessagesHorizontal;
-      }
-      set
-      {
-        this.showSelectedMessagesHorizontal = value;
-        if (this.showSelectedMessagesHorizontal && this.showSelectedMessagesVertical)
-          this.showSelectedMessagesVertical = false;
-        this.OnPropertyChanged();
-      }
-    }
-    private bool showSelectedMessagesHorizontal;
     public bool FollowTail
     {
-      get
-      {
-        return this.followTail;
-      }
-      set
-      {
+      get { return this.followTail; }
+      set {
         this.followTail = value;
         this.OnPropertyChanged();
       }
@@ -63,6 +34,7 @@
     {
       this.followTail = true;
       this.showSearchPanel = false;
+      this.showSelectedMessagesVertical = false;
     }
   }
 }
